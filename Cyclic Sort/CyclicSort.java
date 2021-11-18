224caf4e476fc614341e9218291dc59142f98751
Tushar More <tusharmore@Tushars-MacBook-Air.local>
2021-11-18 23:08:40 +0530

    Cyclic Sort Program has done.

---
+++ Cyclic Sort/CyclicSort.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class CyclicSort {
+    public static void main(String[] args) {
+        
+        int arr[]={5,4,4,2,1};
+        cyclic(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void cyclic(int[] arr){
+
+        int i=0;
+        while(i<arr.length){
+
+            int correctIndex=arr[i]-1;
+
+            if(arr[i]!= arr[correctIndex]){
+                swap(arr,i,correctIndex);
+            }
+
+            else{
+                i++;
+            }
+        }
+    }
+
+    public static void swap(int[] arr,int first,int second){
+        int temp=arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+    }
+}
